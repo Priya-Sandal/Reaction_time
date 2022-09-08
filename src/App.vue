804963637e90cde8ecca962aad64c2e4d19c157e
unknown <add>
2022-09-08 16:27:59 +0530

    add

---
+++ src/App.vue
@@ -0,0 +1,99 @@
+<template>
+
+  <body :class="[clickvar.isActive ? 'blue' : 'green']">
+    <div id="container">
+      <ActionBtn :clickvar="clickvar">
+      </ActionBtn>
+      <Score :clickvar="clickvar">
+      </Score>
+    </div>
+  </body>
+</template>
+
+<script>
+import ActionBtn from './components/ActionBtn.vue'
+import Score from './components/Score.vue'
+export default {
+  components: {
+    ActionBtn,
+    Score
+  },
+  data() {
+    return {
+      clickvar: {
+        show: true,
+        isActive: true,
+        timeStart: 0,
+        timeStop: 0,
+        diff: 0,
+        score: 0,
+        highscore: "No highscore yet.",
+        details: "Click Go to test your reaction time!",
+        firstRun: 1,
+        timeout: 0,
+      }
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+#container {
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.blue {
+  background-color: dodgerblue;
+}
+
+.green {
+  background-color: green;
+}
+
+#details {
+  margin-bottom: 40px;
+}
+
+.actionBtn,
+#score {
+  height: 150px;
+  width: 60vw;
+  border: 1px solid transparent;
+  border-radius: 15px;
+  margin: 20px;
+}
+
+.actionBtn {
+  color: white;
+  font-size: 3em;
+  cursor: pointer;
+}
+
+#go {
+  background-color: mediumseagreen;
+}
+
+#stop {
+  background-color: tomato;
+}
+
+#score {
+  background-color: #d9edf7;
+  padding: 10px 25px 10px 25px;
+  font-size: 1.5em;
+}
+</style>
